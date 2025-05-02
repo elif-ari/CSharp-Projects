01f60ea50e8be855b1a631f3d20830ed5b622087
Elif <43elif1305@gmail.com>
2025-05-02 22:58:34 +0300

    FrmMap duzenlenir

--- Project1_AdonetCustomer/FrmMap.cs
+++ Project1_AdonetCustomer/FrmMap.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace Project1_AdonetCustomer
 {
     public partial class FrmMap : Form
@@ -17,18 +9,40 @@ namespace Project1_AdonetCustomer
             InitializeComponent();
         }
 
+        // Şehir formunu aç
         private void button1_Click(object sender, EventArgs e)
         {
+            // Form zaten açıksa tekrar açma
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is FrmCity)
+                {
+                    form.Focus();
+                    return;
+                }
+            }
+
             FrmCity frmCity = new FrmCity();
             frmCity.Show();
         }
 
+        // Müşteri formunu aç
         private void btnOpenCustomerForm_Click(object sender, EventArgs e)
         {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is FrmCustomer)
+                {
+                    form.Focus();
+                    return;
+                }
+            }
+
             FrmCustomer frmCustomer = new FrmCustomer();
             frmCustomer.Show();
         }
 
+        // Çıkış butonu
         private void btnExit_Click(object sender, EventArgs e)
         {
             var result = MessageBox.Show("Uygulamadan çıkmak istiyor musunuz?", "Çıkış", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
